a9936ca727ad67e813e1c8e7d675e639ba5dcb47
jsturges28 <49821904+jsturges28@users.noreply.github.com>
2022-03-23 14:07:06 -0500

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -11,10 +11,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow.keras as keras
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-from keras.layers import Dense
-from scipy.interpolate import make_interp_spline, BSpline
+from keras.layers import Dense, GaussianNoise
 from mpl_toolkits import mplot3d
 
 x = np.array(np.linspace(-5, 5, 100))
@@ -23,6 +21,40 @@ y = np.array(np.linspace(-5, 5, 100))
 X, Y = np.meshgrid(x, y)
 
 Z = X**4.0 - 22 * X**2.0 + Y**4.0 - 22 * Y ** 2.0
+
+batch_size = 10
+
+input_layer = keras.layers.Input(batch_size = batch_size, shape=(200,))
+dense = Dense(10, activation='relu')(input_layer)
+#gauss = GaussianNoise(stddev=50)(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+output = Dense(100)(dense)
+
+model = keras.Model(inputs=input_layer, outputs=output)
+
+model.compile(loss='mse', optimizer='adam')
+
+X1 = np.hstack((X, Y))
+
+model.fit(X1, Z, epochs=1000, batch_size=10, verbose=0)
+
+zhat = model.predict(X1)
+
+print('MSE: %.3f' % mean_squared_error(Z, zhat))
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, zhat, rstride=1, cstride=1, cmap="viridis", linewidth=0, antialiased=False)
+
+plt.show()
 
 '''
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
@@ -41,46 +73,3 @@ ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="viridis", linewidth=0, anti
 
 plt.show()
 '''
-batch_size = 10
-
-input_layer = keras.layers.Input(batch_size = batch_size, shape=(100,))
-dense = Dense(10, activation='relu')(input_layer)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-dense = Dense(10, activation='relu')(dense)
-output = Dense(100)(dense)
-
-model = keras.Model(inputs=input_layer, outputs=output)
-
-model.compile(loss='mse', optimizer='adam')
-
-#model.summary()
-
-model.fit(X, Z, epochs=1000, batch_size=10, verbose=0)
-
-zhat = model.predict(X)
-
-print('MSE: %.3f' % mean_squared_error(Z, zhat))
-
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
-plt.rcParams["figure.autolayout"] = True
-
-fig = plt.figure()
-
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, zhat, rstride=1, cstride=1, cmap="viridis", linewidth=0, antialiased=False)
-
-plt.show()
