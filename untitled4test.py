565c96557508dfa13c1ab3bd7299f49758e74c1a
jsturges28 <49821904+jsturges28@users.noreply.github.com>
2022-03-24 17:58:32 -0500

    Add files via upload

---
+++ untitled4test.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 17:30:28 2022
+
+@author: jstur2828
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras as keras
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense, GaussianNoise
+from scipy.interpolate import make_interp_spline, BSpline
+
+
+
+x = [i for i in range(-5, 5)]
+x1 = np.asarray(x)
+
+xnew = np.linspace(-5.5, 5.5, 300)
+
+y = [(i**4.0 - 22 * (i ** 2.0)) for i in x]
+
+xnew = np.linspace(-5.5, 5.5, 300)
+spl = make_interp_spline(x, y, k=3)
+power_smooth = spl(xnew)
+
+y = [(i**4.0 - 22 * (i ** 2.0)) for i in xnew]
+y1 = np.asarray(y)
+
+input_layer = keras.layers.Input(shape=(1,))
+dense = Dense(10, activation='relu')(input_layer)
+gauss = GaussianNoise(stddev=25)(dense)
+dense = Dense(10, activation='relu')(gauss)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+output = Dense(1)(dense)
+
+model = keras.Model(inputs=input_layer, outputs=output)
+
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(xnew, y1, epochs=1000, batch_size=10, verbose=0)
+
+yhat = model.predict(xnew)
+
+print('MSE: %.3f' % mean_squared_error(y1, yhat))
+
+spl1 = make_interp_spline(xnew, yhat, k=3)
+power_smooth1 = spl1(xnew)
+
+plt.plot(xnew,power_smooth1, label='Predicted')
+plt.plot(xnew,power_smooth, label = 'Actual')
+plt.title('Input (x) versus Output (y)')
+plt.xlabel('Input Variable (x)')
+plt.ylabel('Output Variable (y)')
+plt.legend()
+plt.show()
