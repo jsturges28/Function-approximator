9dbd25198add1ce7631e0dabba8362fff1aa165f
jsturges28 <49821904+jsturges28@users.noreply.github.com>
2022-04-07 22:35:44 -0500

    Add files via upload

---
+++ Function 1 ANN No Noise.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Function approximator for problem 1
+
+Created on Tue Mar 22 18:33:19 2022
+
+@author: jstur2828
+"""
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras as keras
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense
+from scipy.interpolate import make_interp_spline, BSpline
+
+# Function to save raw model data (loss for each epoch)
+
+def saveHistory():
+    hist_df = pd.DataFrame(history.history) 
+    hist_csv_file = 'history_function1.csv'
+    with open(hist_csv_file, mode='w') as f:
+        hist_df.to_csv(f)
+
+# Create a range of x values from -5 to 5
+
+x = [i for i in range(-5, 5)]
+x1 = np.asarray(x)
+
+# Create a new range of values in order to plot a smooth quadratic line
+xnew = np.linspace(-5.5, 5.5, 300)
+
+# Calculate the predicted values in y
+
+y = [(i**4.0 - 22 * (i ** 2.0)) for i in x]
+
+# Make a spline in order to smoothly fit a quadratic function
+
+xnew = np.linspace(-5.5, 5.5, 300)
+spl = make_interp_spline(x, y, k=3)
+power_smooth = spl(xnew)
+
+y = [(i**4.0 - 22 * (i ** 2.0)) for i in xnew]
+y1 = np.asarray(y)
+
+# Create model 
+
+input_layer = keras.layers.Input(shape=(1,))
+dense = Dense(10, activation='relu')(input_layer)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+output = Dense(1)(dense)
+
+# Build model
+
+model = keras.Model(inputs=input_layer, outputs=output)
+
+# Compile model 
+
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+
+# Fit and run the model and save to history object for later use
+
+history = model.fit(xnew, y1, epochs=1000, batch_size=10, verbose=0)
+
+saveHistory()
+
+# Make predictions for y
+
+yhat = model.predict(xnew)
+
+print('MSE: %.3f' % mean_squared_error(y1, yhat))
+
+# Plot the predicted vs. actual values on a 2D graph
+
+spl1 = make_interp_spline(xnew, yhat, k=3)
+power_smooth1 = spl1(xnew)
+
+plt.plot(xnew,power_smooth1, label='Predicted')
+plt.plot(xnew,power_smooth, label = 'Actual')
+plt.title('Input (x) versus Output (y)')
+plt.xlabel('Input Variable (x)')
+plt.ylabel('Output Variable (y)')
+plt.legend()
+plt.show()
+
+# Plot the loss on a graph
+
+plt.plot(history.history['loss'])
+plt.title('function 1 loss (without noise)')
+plt.ylabel('loss')
+plt.xlabel('number of epochs')
+plt.show()
---
+++ Function 2 ANN Noise.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Function approximator for problem 2
+
+Created on Tue Mar 22 19:59:59 2022
+
+@author: jstur2828
+"""
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras as keras
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense, GaussianNoise
+from mpl_toolkits import mplot3d
+
+# Function to save raw model data (loss for each epoch)
+
+def saveHistory():
+    hist_df = pd.DataFrame(history.history) 
+    hist_csv_file = 'history_function2.csv'
+    with open(hist_csv_file, mode='w') as f:
+        hist_df.to_csv(f)
+
+# Create two evenly spaced intervals for x and y
+
+x = np.array(np.linspace(-5, 5, 100))
+y = np.array(np.linspace(-5, 5, 100))
+
+# Create a grid of x and y values (numpy meshgrid works well with linspace arrays)
+
+X, Y = np.meshgrid(x, y)
+
+# Create function that we will be predicting against
+
+Z = X**4.0 - 22 * X**2.0 + Y**4.0 - 22 * Y ** 2.0
+
+batch_size = 10
+
+# Create model (and add noise)
+
+input_layer = keras.layers.Input(shape=(200,))
+dense = Dense(10, activation='relu')(input_layer)
+gauss = GaussianNoise(stddev=50)(dense)
+dense = Dense(10, activation='relu')(gauss)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+dense = Dense(10, activation='relu')(dense)
+output = Dense(100)(dense)
+
+# Build model
+
+model = keras.Model(inputs=input_layer, outputs=output)
+
+# Compile model
+
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+# "Stack" the X and Y grids to produce a sequence that represents a
+# Cartesian product that can be read by the computer
+
+X1 = np.hstack((X, Y))
+
+# Fit and run the model and save to history object for later use
+
+history = model.fit(X1, Z, epochs=10000, batch_size=10, verbose=0)
+
+saveHistory()
+
+# Make predictions for Z
+
+zhat = model.predict(X1)
+
+print('MSE: %.3f' % mean_squared_error(Z, zhat))
+
+# Plot the predictions on a 3D grid
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, zhat, rstride=1, cstride=1, cmap="viridis", linewidth=0, antialiased=False)
+
+plt.show()
+
+# Plot the loss on a graph
+
+plt.plot(history.history['loss'])
+plt.title('function 2 loss (no noise)')
+plt.ylabel('loss')
+plt.xlabel('number of epochs')
+plt.show()
